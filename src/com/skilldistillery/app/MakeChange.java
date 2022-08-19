9e63b8c15d5ccd9f0b7728c8a6f48476d668bddb
Josh Ingram <josh@Joshs-MacBook-Pro.local>
2022-08-19 17:11:15 -0600

    first draft of bills only

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -8,7 +8,7 @@ public class MakeChange {
 		System.out.println("** Cash Register **");
 		System.out.println();
 		// Get an item's price and how much the customer paid
-		double purchasePrice, amountTendered;
+		double purchasePrice, amountTendered, amountOver;
 		Scanner kb = new Scanner(System.in);
 		System.out.print("What is the price of the item being purchased?:");
 				purchasePrice = kb.nextDouble();
@@ -19,16 +19,40 @@ public class MakeChange {
 			System.out.println("No change due, thank the customer for their business.");
 		}
 		else if (amountTendered < purchasePrice) {
-			System.out.println("Insufficient funds tendered, ask the customer for more money");
+			System.out.println("Insufficient funds tendered, ask the customer for more money.");
 		}
 		else {
-			
-		}
-			
-			
-		
+			System.out.println("Change is due:");
+		// Determine the amount of change due
+			double amountToBreakDown = amountTendered - purchasePrice;  //amountToBreaDown was amountOver
+			System.out.println(amountToBreakDown); //amountToBreaDown was amountOver
+			//String changeOutPut = whatChangeDue(amountOver);
+		//}
 		
 kb.close();
-	}
+	//}
 
+	//public static String whatChangeDue(double amountToBreakDown) {
+		double num20b = amountToBreakDown / 20;
+			int num20bO = (int)num20b;
+		double running = amountToBreakDown - (num20bO * 20);
+		double num10b = running / 10;
+			int num10bO = (int)num10b;
+		running = running - (num10bO * 10);
+		
+		double num5b = running / 5;
+		int num5bO = (int)num5b;
+		running = running - (num5bO * 5);
+		
+		double num1b = running / 1;
+		int num1bO = (int)num1b;
+		running = running - (num5bO * 1);
+		
+		System.out.println("The customer gets the following: " + num20bO + " 20 bills, \n"
+				+ num10bO + " 10 bills, \n"
+				+ num5bO + " 5 bills, \n"
+				+ num1bO + " 1 bills, \n");  
+
+	}
+	}
 }
