f8544b9669b6851fdff7dbe493106ca4300ccaeb
Josh Ingram <josh@Joshs-MacBook-Pro.local>
2022-08-19 16:30:25 -0600

    initial menu screens added

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,10 +1,34 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("** Cash Register **");
+		System.out.println();
+		// Get an item's price and how much the customer paid
+		double purchasePrice, amountTendered;
+		Scanner kb = new Scanner(System.in);
+		System.out.print("What is the price of the item being purchased?:");
+				purchasePrice = kb.nextDouble();
+		System.out.print("How much did the customer pay?: ");	
+			amountTendered = kb.nextDouble();
+		// Compare amount tendered to price	
+		if (amountTendered == purchasePrice) {
+			System.out.println("No change due, thank the customer for their business.");
+		}
+		else if (amountTendered < purchasePrice) {
+			System.out.println("Insufficient funds tendered, ask the customer for more money");
+		}
+		else {
+			
+		}
+			
+			
+		
+		
+kb.close();
 	}
 
 }
