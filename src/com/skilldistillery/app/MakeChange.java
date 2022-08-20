30e8a98f1f6153f192cdd7f7afe2221f513a9ddd
Josh Ingram <josh@Joshs-MacBook-Pro.local>
2022-08-19 21:14:56 -0600

    cleaned up redundant int casting

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -9,105 +9,97 @@ public class MakeChange {
 		System.out.println();
 		menu();
 	}
+
 	public static void menu() {
-		// Get an item's price and how much the customer paid
+		// Retrieve an item's price and how much the customer paid
 		double purchasePrice, amountTendered, excess;
 		Scanner kb = new Scanner(System.in);
 		System.out.print("What is the price of the item being purchased?:");
-				purchasePrice = kb.nextDouble();
+		purchasePrice = kb.nextDouble();
 
 //				if (purchasePrice <= 0) {
 //					System.out.println("Call a manager, price is $0 or less");
 //				}
-		System.out.print("How much did the customer pay?: ");	
-			amountTendered = kb.nextDouble();
-		// Compare amount tendered to price	
+		System.out.print("How much did the customer pay?: ");
+		amountTendered = kb.nextDouble();
+		// Compare amount tendered to price
 		if (amountTendered == purchasePrice) {
 			System.out.println("No change due, thank the customer for their business.");
-		}
-		else if (amountTendered < purchasePrice) {
+		} else if (amountTendered < purchasePrice) {
 			System.out.println("Insufficient funds tendered, ask the customer for more money.");
-		}
-		else {
+		} else {
 			System.out.print("Change is due:");
-			excess = amountTendered - purchasePrice; 
-			whatChangeDue(excess);	
+			excess = amountTendered - purchasePrice;
+			whatChangeDue(excess);
 		}
-	
+
 		kb.close();
-		
+
 	}
 
 	public static void whatChangeDue(double amountOver) {
-		//Determine the amount of change due and convert to rounded int
-		int changeInt = (int)(amountOver * 100 + .5);
+		// Determine the amount of change due and convert to rounded int
+		int changeInt = (int) (amountOver * 100 + .5);
 		double changeDouble = changeInt;
-		System.out.println("$ " + (changeDouble/100)); 
-		
-		//number of $20 bills 
+		System.out.println("$ " + (changeDouble / 100));
+		// Note: values below this point are multiplied by 100
+
+		// number of $20 bills
 		int num20 = changeInt / 2000;
-			int num20Bills = (int)num20;
-		int running = changeInt - (num20Bills * 2000);
-	
-		//number of $10 bills
+		int running = changeInt - (num20 * 2000);
+
+		// number of $10 bills
 		int num10 = running / 1000;
-			int num10Bills = (int)num10;
-		running = running - (num10Bills * 1000);
-	
-		//number of $5 bills
+		running = running - (num10 * 1000);
+
+		// number of $5 bills
 		int num5 = running / 500;
-		int num5Bills = (int)num5;
-		running = running - (num5Bills * 500);
-	
-		//number of $1 bills
+		running = running - (num5 * 500);
+
+		// number of $1 bills
 		int num1 = running / 100;
-		int num1Bills = (int)num1;
-		running = running - (num1Bills * 100);
-		
-		//number of Quarters
+		running = running - (num1 * 100);
+
+		// number of Quarters
 		int num_25c = running / 25;
-		int numQuarters = (int)num_25c;
-		running = running - (numQuarters * 25);
-		
-		//number of Dimes
+		running = running - (num_25c * 25);
+
+		// number of Dimes
 		int num_10c = running / 10;
-		int numDimes = (int)num_10c;
-		running = running - (numDimes * 10);
-	
-		//number of Nickels
+		running = running - (num_10c * 10);
+
+		// number of Nickels
 		int num_5c = running / 5;
-		int numNickels = (int)num_5c;
-		running = running - (numNickels * 5);
-		
-		//number of Pennies
+		running = running - (num_5c * 5);
+
+		// number of Pennies
 		int num_1c = running / 1;
-		int numPennies = (int)num_1c;
-		
-		//Printout the results
-		System.out.println("The customer gets the following change: "); 
-		if (num20Bills > 0)	{	
-		System.out.println(num20Bills + " 20 bill(s)");
+
+		// Printout the results
+		System.out.println("Give the customer the following change: ");
+		if (num20 > 0) {
+			System.out.println(num20 + " $20 bill(s)");
 		}
-		if (num10Bills > 0) {
-		System.out.println(num10Bills + " 10 bill(s)");
+		if (num10 > 0) {
+			System.out.println(num10 + " $10 bill(s)");
 		}
-		if (num5Bills > 0) {
-		System.out.println(num5Bills + " 5 bill(s)");
+		if (num5 > 0) {
+			System.out.println(num5 + " $5 bill(s)");
 		}
-		if (num1Bills > 0) {
-			System.out.println(num1Bills + " 1 bill(s)");
+		if (num1 > 0) {
+			System.out.println(num1 + " $1 bill(s)");
 		}
-		if (numQuarters > 0)	{
-			System.out.println(numQuarters + " quarter(s)");
+		if (num_25c > 0) {
+			System.out.println(num_25c + " quarter(s)");
 		}
-		if (numDimes > 0)	{
-			System.out.println(numDimes + " dime(s)");
+		if (num_10c > 0) {
+			System.out.println(num_10c + " dime(s)");
 		}
-		if (numNickels > 0) {
-			System.out.println(+ numNickels + " nickel(s)");
+		if (num_5c > 0) {
+			System.out.println(+num_5c + " nickel(s)");
 		}
-		if (numPennies > 0 ) {
-			System.out.println(numPennies + " penny/pennies");
-		}		
-    }
+		if (num_1c > 0) {
+			System.out.println(num_1c + " penny/pennies");
+		}
+	}
 }
